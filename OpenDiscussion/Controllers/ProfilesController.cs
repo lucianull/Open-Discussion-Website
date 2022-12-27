9b6ec2be93d872ab596147889c32143820a0a416
Eronate <kitidudu@yahoo.com>
2022-12-27 14:38:33 +0200

    image

--- OpenDiscussion/Controllers/ProfilesController.cs
+++ OpenDiscussion/Controllers/ProfilesController.cs
@@ -15,7 +15,6 @@ namespace OpenDiscussion.Controllers
 
         public IActionResult GetId(string id)
         {
-            Console.WriteLine('\n' + id + '\n');
             int profileId = db.Users.Include("Profile").Where(u => u.Id == id).First().Profile.ProfileId;
             return RedirectToAction("Show", new {id = profileId});
 
@@ -24,6 +23,20 @@ namespace OpenDiscussion.Controllers
         {
             Profile prof = db.Profiles.Find(id); //profilul userului
             var currentUser = db.Users.Include("Profile").Where(u => u.Id == prof.ApplicationUserId).First();
+            return View(currentUser);
+        }
+
+        [HttpPost]
+        public IActionResult Show(int id, [FromForm] ApplicationUser requestUser) //id va fi id-ul profilului
+        {
+            Profile prof = db.Profiles.Find(id); //profilul userului
+            var currentUser = db.Users.Include("Profile").Where(u => u.Id == prof.ApplicationUserId).First();
+            if (ModelState.IsValid)
+            {
+                prof.Description = requestUser.Profile.Description;
+                prof.Avatar = requestUser.Profile.Avatar;
+                db.SaveChanges();
+            }
             return View(currentUser);
         }
     }
