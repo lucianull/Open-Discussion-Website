8f9c8548fcba246b63b0d831c0c4be8e3df0671e
Moisii Lucian <lucianmoisii31@gmail.com>
2023-01-10 00:59:10 +0200

    final home page

--- OpenDiscussion/Controllers/HomeController.cs
+++ OpenDiscussion/Controllers/HomeController.cs
@@ -3,9 +3,16 @@ using Microsoft.AspNetCore.Mvc;
 using OpenDiscussion.Data;
 using OpenDiscussion.Models;
 using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
 
 namespace OpenDiscussion.Controllers
 {
+    public struct discussionDetails
+    {
+        public string discussionTitle, discussionContent, topicName, categoryName;
+        public DateTime discussionDate;
+        public int nrComments, discussionId;
+    }
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -31,13 +38,30 @@ namespace OpenDiscussion.Controllers
         {
             if (User.Identity.IsAuthenticated)
                 return RedirectToAction("Index", "Categories");
-            var Discussions = (from discussion in db.Discussions select discussion);
+            var Discussions = (from discussion in db.Discussions
+                               join topic in db.Topics
+                                    on discussion.TopicId equals topic.TopicId into discussionDet
+                               from disc in discussionDet
+                               join category in db.Categories
+                                    on disc.CategoryId equals category.CategoryId
+                               select new {discussionId = discussion.DiscussionId, discussionTitle = discussion.Title, discussionContent = discussion.Content, discussionDate = discussion.Date, topicName = disc.Name, categoryName = category.Name}).ToList();
+            //var Discussions = (from discussion in db.Discussions select discussion);
             int N = Discussions.Count();
             Random random = new Random();
-            List<Discussion> discutii = new List<Discussion>();
-            discutii.Add(Discussions.Skip(random.Next(0, N)).Take(1).First());
-            discutii.Add(Discussions.Skip(random.Next(0, N)).Take(1).First());
-            discutii.Add(Discussions.Skip(random.Next(0, N)).Take(1).First());
+            List<discussionDetails> discutii = new List<discussionDetails>();
+            for(int k = 1; k <= 3; k++)
+            {
+                int randomNumber = random.Next(0, N);
+                discutii.Add(new discussionDetails
+                {
+                    discussionTitle = Discussions[randomNumber].discussionTitle,
+                    discussionContent = Discussions[randomNumber].discussionContent,
+                    discussionDate = Discussions[randomNumber].discussionDate,
+                    topicName = Discussions[randomNumber].topicName,
+                    categoryName = Discussions[randomNumber].categoryName,
+                    nrComments = db.Comments.Where(comment => comment.DiscussionId == Discussions[randomNumber].discussionId).Count()
+                });
+            }
             ViewBag.DisplayArticles = discutii;
             return View();
         }
