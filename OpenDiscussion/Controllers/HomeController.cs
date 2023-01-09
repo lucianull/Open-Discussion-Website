f935669a021dd71b63d0cc8299e834ee3885345c
Moisii Lucian <lucianmoisii31@gmail.com>
2023-01-09 16:15:04 +0200

    added basic home page

--- OpenDiscussion/Controllers/HomeController.cs
+++ OpenDiscussion/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OpenDiscussion.Data;
 using OpenDiscussion.Models;
 using System.Diagnostics;
 
@@ -7,15 +9,29 @@ namespace OpenDiscussion.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext db;
+        private readonly UserManager <ApplicationUser> _userManager;
+        private readonly RoleManager < IdentityRole > _roleManager;
+        public HomeController(ApplicationDbContext context,
+            ILogger<HomeController> logger,
+            UserManager <ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
         {
+            db = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
-
+            if (User.Identity.IsAuthenticated)
+                return RedirectToAction("Index", "Categories");
+            var Discussions = (from discussion in db.Discussions select discussion);
+            Random random= new Random();
+            int DiscussionIndex = random.Next(1, Discussions.Count());
+            Discussion discutie = Discussions.Skip(DiscussionIndex).Take(1).First();
+            ViewBag.DisplayArticle = discutie;
             return View();
         }
 
